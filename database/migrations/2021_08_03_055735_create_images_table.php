566092d106591e67796c24f1592d9bd3e1ff1b4c
dzntech <dzn_tech@inveskey.com>
2021-08-05 15:14:09 +0530

    image table migration fixed

--- database/migrations/2021_08_03_055735_create_images_table.php
+++ database/migrations/2021_08_03_055735_create_images_table.php
@@ -15,7 +15,6 @@ class CreateImagesTable extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id')->unsigned();
             $table->string('title');
             $table->string('image');
             $table->string('description');
